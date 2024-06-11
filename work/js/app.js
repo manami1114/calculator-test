4358ebc95f7f6c1489dbd76a9597cbaa6c5116c8
manami1114 <o2ka.mgmg@gmail.com>
2024-06-11 21:16:45 +0900

    1st commit

---
+++ work/js/app.js
@@ -0,0 +1,25 @@
+const display = document.getElementById('display');
+
+function getCalc(btn) {
+  const dataNumber = btn.dataset.number;
+  const dataOperator = btn.dataset.operator;
+  const dataClear = btn.dataset.clear;
+  const dataEquals = btn.dataset.equals;
+  if (dataEquals === "=") {
+    display.value = eval(display.value);
+  } else if (dataClear === "all") {
+    display.value = "";
+  } else {
+    if (dataOperator) {
+      if (dataOperator === "*") {
+        display.value += "*";
+      } else if (dataOperator === "/") {
+        display.value += "/";
+      } else {
+        display.value += dataOperator;
+      }
+    } else if (dataNumber) {
+      display.value += dataNumber;
+    }
+  }
+}
